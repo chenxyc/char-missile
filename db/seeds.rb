4fa27245f70f1391cfef99f24c4cf43dbc355f41
Charmaine <charmainechenxy@gmail.com>
2016-08-14 07:20:28 +0000

    Initial Commit

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+
+Mayor.create(name: 'Emanuel', city: cities.first)
+Todo.create!(title: 'walk the dog', notes: 'walk to the new park.')
+Todo.create!(title: 'do laundry')
+Todo.create!(title: 'go grocery shopping', notes: 'milk, eggs, bread, cheese')
